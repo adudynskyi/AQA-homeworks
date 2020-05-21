bd2e86f214914d3a2b3bc9a767e5d39b2a2dc51b
adudynskyi <oleksandr.dudynskyi@ring.com>
2020-05-21 18:01:24 +0300

    Add files via upload
    Appium Assignment added

---
+++ intro-to-appium-hw/src/main/java/com/example/appium/PageView.java
@@ -0,0 +1,30 @@
+package com.example.appium;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageView {
+    AppiumDriver<MobileElement> driver;
+
+    @AndroidFindBy(id = "edit")
+    @iOSXCUITFindBy(iOSNsPredicate = "name == 'IntegerA'")
+    private MobileElement textField;
+
+    public PageView(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public String getTextField() {
+        return textField.getText();
+    }
+
+    public PageView setTextField(String text) {
+        textField.sendKeys(text);
+        return this;
+    }
+}
---
+++ intro-to-appium-hw/src/test/java/com/example/appium/SampleAppTest.java
@@ -0,0 +1,67 @@
+package com.example.appium;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static java.lang.System.getenv;
+import static org.testng.Assert.assertEquals;
+
+public class SampleAppTest {
+    private AppiumDriverLocalService server;
+    private AppiumDriver<MobileElement> driver;
+
+    @BeforeClass
+    private void setUp() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String platform = getenv("APPIUM_DRIVER");
+        platform = platform == null ? "ANDROID" : platform.toUpperCase();
+        String path = System.getProperty("user.dir");
+
+        if (platform.equals("ANDROID")) {
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_3_XL");
+            capabilities.setCapability(MobileCapabilityType.APP, path + "/ApiDemos-debug.apk");
+
+            server = new AppiumServiceBuilder().usingAnyFreePort().build();
+            server.start();
+            driver = new AndroidDriver<>(server, capabilities);
+
+            ((AndroidDriver<MobileElement>) driver).startActivity(new Activity("io.appium.android.apis", ".view.TextFields"));
+        } else {
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.3.1");
+            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCuiTest");
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11 (13.3)");
+            capabilities.setCapability(MobileCapabilityType.UDID, "2FC4280D-EB6C-4306-B76A-82B6D37CFD1B");
+            capabilities.setCapability(MobileCapabilityType.APP, path + "/TestApp.app.zip");
+
+            server = new AppiumServiceBuilder().usingAnyFreePort().build();
+            server.start();
+            driver = new IOSDriver<>(server, capabilities);
+        }
+    }
+
+    @Test
+    public void textFieldTest() {
+        PageView view = new PageView(driver);
+        view.setTextField("text");
+
+        assertEquals(view.getTextField(), "text", "Text is wrong");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+        server.stop();
+    }
+}
